73bf4fcfc2506189b561205171d1c967f5beebff
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-04-23 21:33:09 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,7 @@
 // Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
 //  после чего вставит все li за одну операцию в список ul.ingredients.
 //  Для создания DOM - узлов используй document.createElement().
+
 const allElOfMainList = document.querySelectorAll('.item'); // Объект NodeList 
 console.log(`В списке ${allElOfMainList.length} категории`)
 
