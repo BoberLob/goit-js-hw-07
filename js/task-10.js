132acc4dc040eebd1adf4c63d0c8a396c50c1d91
BoberLob <55806787+BoberLob@users.noreply.github.com>
2021-07-03 06:49:01 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,6 @@
+// Что будет выведено в консоль?
+console.log('#####:smthg', smthg);
+                
 const fn = arr => arr.map(el => {
     const item = document.createElement('div');
     item.textContent = el;
@@ -5,4 +8,27 @@ const fn = arr => arr.map(el => {
     return item;
    });
   
-  console.log(fn(['html', 'css', 'js', 'react']));
+  //console.log(fn(['html', 'css', 'js', 'react']));
+
+console.log('#####fn(['html', 'css', 'js', 'react'])', fn(['html', 'css', 'js', 'react']))
+                
+// Что будет выведено в консоль?
+  const person = {
+    age: 10,
+    setAge(newAge) {
+      this.age = newAge;
+    },
+    refreshAge(userId) {
+      fetchAgeFromDb(function(newAge) {
+        this.setAge(newAge);
+      });
+    },
+  };
+  
+  function fetchAgeFromDb(cb) {
+   cb(20);
+  }
+  
+  person.refreshAge();
+  console.log(person.age);
+//console.log('#####:person.age', person.age);
